4aaeff64aa4f13192f071022104935579607b083
Michael Calkins <m.calkins576@gmail.com>
2019-11-04 20:40:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 # Write your code here.
 def line(line)
+  new_line = ""
   if line.empty?
     puts "The line is currently empty."
   else
-    puts line
+    line.each_with_index do |person, index|
+    new_line << "The line is currently"
   end
 end
