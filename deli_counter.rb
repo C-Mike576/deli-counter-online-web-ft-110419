33a7a94a88115cff5505c94def55fb4119ca6ca6
Michael Calkins <m.calkins576@gmail.com>
2019-11-04 20:58:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(line)
   new_line = ""
   if line.empty?
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     new_line << "The line is currently:"
     line.each_with_index do |person, index|
