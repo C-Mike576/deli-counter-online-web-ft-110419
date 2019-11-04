91e5fe6380e7ec5a7d6f9553fdafd0fef1b8c872
Michael Calkins <m.calkins576@gmail.com>
2019-11-04 20:52:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,7 @@ def line(line)
   else
     new_line << "The line is currently: "
     line.each_with_index do |person, index|
-      new_line << "#{index+1}. #{person} " unless person == line[-1] do
-        new_line << "#{index+1}. #{person}"
-      end
+      new_line << "#{index+1}. #{person} "
     end
   end
  p new_line
